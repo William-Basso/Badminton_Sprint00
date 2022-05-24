3b18bce5eddd735ca8d53ab5e8b944ccca69691c
William-Basso <94117061+William-Basso@users.noreply.github.com>
2022-05-23 23:22:58 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import './index.css';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+
+function App() {
+  return (
+    <React.Fragment>
+      <div id="principal">
+        <div id="cab">
+          <h4 id="l1_c1" className='centerTitle'>Geral</h4>
+          <h4 id="l1_c2" className='centerTitle'>Fora da Quadra</h4>
+          <div id="l2_c1">
+            <p>Nome Jogador/Equipe: </p>
+            <p>Total de Pontos: </p>
+            <p>Fora da Quadra:</p>
+          </div>
+          <div id="l2_c2">
+            <div id="colum1">
+              <p>Saque:</p>
+              <p>Forehand:</p>
+              <p>Backhand:</p>
+            </div>
+            <div id="colum2">
+              <p>Clear: </p>
+              <p>Drop:</p>
+              <p>Smash: </p>
+            </div>
+            <div id="colum3">
+              <p>Drive: </p>
+              <p>Lob: </p>
+              <p>Net Shot: </p>
+            </div>
+            <div id="colum4">
+              <p>Net Kill: </p>
+              <p>Net Lift: </p>
+            </div>
+          </div>
+
+        </div>
+
+        <div id="area">
+          <div id="wrapArea">
+            <div id="area_1" className='area'>
+              <h2 className='titulo'>Area 1</h2>
+              <div className="wrapDados">
+                <div id="dados" className="dados">
+                  <p>Saque:     </p>
+                  <p>Forehand:  </p>
+                  <p>Backhand:  </p>
+                  <p>Clear:     </p>
+                  <p>Drop:      </p>
+                  <p>Smash:     </p>
+                  <p>Drive:     </p>
+                  <p>Lob:       </p>
+                  <p>Net Shot:  </p>
+                  <p>Net Kill:  </p>
+                  <p>Net Lift:  </p>
+                  <p>Ponto:     </p>
+                </div>
+              </div>
+            </div>
+            <div id="area_2" className='area'>
+              <h2 className='titulo'>Area 2</h2>
+              <div className="wrapDados">
+                <div id="dados" className="dados">
+                  <p>Saque:     </p>
+                  <p>Forehand:  </p>
+                  <p>Backhand:  </p>
+                  <p>Clear:     </p>
+                  <p>Drop:      </p>
+                  <p>Smash:     </p>
+                  <p>Drive:     </p>
+                  <p>Lob:       </p>
+                  <p>Net Shot:  </p>
+                  <p>Net Kill:  </p>
+                  <p>Net Lift:  </p>
+                  <p>Ponto:     </p>
+                </div>
+              </div>
+            </div>
+            <div id="area_3" className='area'>
+              <h2 className="titulo">Area 3</h2>
+              <div className="wrapDados">
+                <div id="dados" className="dados">
+                  <p>Saque:    </p>
+                  <p>Forehand: </p>
+                  <p>Backhand: </p>
+                  <p>Clear:    </p>
+                  <p>Drop:     </p>
+                  <p>Smash:    </p>
+                  <p>Drive:    </p>
+                  <p>Lob:      </p>
+                  <p>Net Shot: </p>
+                  <p>Net Kill: </p>
+                  <p>Net Lift: </p>
+                  <p>Ponto:    </p>
+                </div>
+              </div>
+            </div>
+            <div id="area_4" className='area'>
+              <h2 className="titulo">Area 4</h2>
+              <div className="wrapDados">
+                <div id="dados" className="dados">
+                  <p>Saque:     </p>
+                  <p>Forehand:  </p>
+                  <p>Backhand:  </p>
+                  <p>Clear:     </p>
+                  <p>Drop:      </p>
+                  <p>Smash:     </p>
+                  <p>Drive:     </p>
+                  <p>Lob:       </p>
+                  <p>Net Shot:  </p>
+                  <p>Net Kill:  </p>
+                  <p>Net Lift:  </p>
+                  <p>Ponto:     </p>
+                </div>
+              </div>
+            </div>
+            <div id="area_5" className='area' >
+              <h2 className="titulo">Area 5</h2>
+              <div className="wrapDados">
+                <div id="dados" className="dados">
+                  <p>Saque:     </p>
+                  <p>Forehand:  </p>
+                  <p>Backhand:  </p>
+                  <p>Clear:     </p>
+                  <p>Drop:      </p>
+                  <p>Smash:     </p>
+                  <p>Drive:     </p>
+                  <p>Lob:       </p>
+                  <p>Net Shot:  </p>
+                  <p>Net Kill:  </p>
+                  <p>Net Lift:  </p>
+                  <p>Ponto:     </p>
+                </div>
+              </div>
+            </div>
+            <div id="area_6" className='area'>
+              <h2 className="titulo">Area 6</h2>
+              <div className="wrapDados">
+                <div id="dados" className="dados">
+                  <p>Saque:     </p>
+                  <p>Forehand:  </p>
+                  <p>Backhand:  </p>
+                  <p>Clear:     </p>
+                  <p>Drop:      </p>
+                  <p>Smash:     </p>
+                  <p>Drive:     </p>
+                  <p>Lob:       </p>
+                  <p>Net Shot:  </p>
+                  <p>Net Kill:  </p>
+                  <p>Net Lift:  </p>
+                  <p>Ponto:     </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </React.Fragment >
+  );
+}
+
+export default App;
